224ce296093764ec931c861903a891a2c26a0868
Tony Winslow <winslowt1@nku.edu>
2015-11-19 19:43:41 -0500

    Initial Blocstagram commit

---
+++ Blocstagram/Blocstagram/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Tony  Winslow on 11/19/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+
+@end
+
